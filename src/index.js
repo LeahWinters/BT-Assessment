c21a95cb7f95ff9d13946f5b615f731a4d71d92d
Leah Winters <leah.winters24@gmail.com>
2020-11-24 18:01:59 -0500

    Fetch and display members and public members data/error message with minimal styling

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ var repoIdsDiv = document.querySelector(".repos-id-div");
 var eventsIdsDiv = document.querySelector(".events-id-div");
 var hooksIdsDiv = document.querySelector(".hooks-id-div");
 var issuesIdsDiv = document.querySelector(".issues-id-div");
+var membersIdsDiv = document.querySelector(".members-id-div");
+var publicMembersIdsDiv = document.querySelector(".public-members-id-div");
 
 let reposData;
 let eventsData;
 let hooksData;
 let issuesData;
+let membersData;
+let publicMembersData;
 
 reposData = fetch("https://api.github.com/orgs/BoomTownROI/repos")
   .then((data) => data.json())
@@ -28,18 +32,32 @@ issuesData = fetch("https://api.github.com/orgs/BoomTownROI/issues")
   .then((data) => data.message)
   .catch((error) => console.log(error.message));
 
-Promise.all([reposData, eventsData, hooksData, issuesData])
+membersData = fetch("https://api.github.com/orgs/BoomTownROI/members{/member}")
+  .then((data) => data.json())
+  .then((data) => data.message)
+  .catch((error) => console.log(error.message));
+
+publicMembersData = fetch("https://api.github.com/orgs/BoomTownROI/public_members{/member}")
+  .then((data) => data.json())
+  .then((data) => data.message)
+  .catch((error) => console.log(error.message));
+
+Promise.all([reposData, eventsData, hooksData, issuesData, membersData, publicMembersData])
   .then((data) => {
     reposData = data;
     eventsData = data;
     hooksData = data;
     issuesData = data;
+    membersData = data;
+    publicMembersData = data;
   })
   .then(() => {
     displayReposIds(reposData);
     displayEventsIds(eventsData);
     displayHooksErrorMessage(hooksData);
     displayIssuesErrorMessage(issuesData);
+    displayMembersErrorMessage(membersData);
+    displayPublicMembersErrorMessage(publicMembersData)
   })
   .catch((error) => {
     console.log(error.message);
@@ -60,37 +78,67 @@ const displayEventsIds = (events) => {
       <section class="individual-event">
         <p>Event ID: ${event.id}<p>
       </section>`;
-  }); 
+  });
 };
 
 const displayHooksErrorMessage = (hooks) => {
   if (hooks[2] === "Not Found") {
-     hooksIdsDiv.innerHTML = `<section class="individual-hook">
+    hooksIdsDiv.innerHTML = `<section class="individual-hook">
         <p>No Hooks Found<p>
       </section>`;
-      console.log(hooks[2]);
   } else {
     hooksIdsDiv.innerHTML = hooks[2].map((hook) => {
       return `
       <section class="individual-event">
         <p>Hook ID: ${hook.id}<p>
       </section>`;
-    })
+    });
   }
-}
+};
 
 const displayIssuesErrorMessage = (issues) => {
   if (issues[3] === "Not Found") {
-     issuesIdsDiv.innerHTML = `<section class="individual-issue">
+    issuesIdsDiv.innerHTML = `<section class="individual-issue">
         <p>No Issues Found<p>
       </section>`;
-      console.log(issue[3]);
   } else {
     issuesIdsDiv.innerHTML = issues[3].map((issue) => {
       return `
       <section class="individual-issue">
         <p>Issue ID: ${issue.id}<p>
       </section>`;
-    })
+    });
   }
-}
+};
+
+const displayMembersErrorMessage = (members) => {
+  if (members[4] === "Not Found") {
+    membersIdsDiv.innerHTML = `<section class="individual-member">
+       <p>No Members Found<p>
+     </section>`;
+    console.log(members[4]);
+  } else {
+    membersIdsDiv.innerHTML = members[4].map((member) => {
+      return `
+     <section class="individual-member">
+       <p>Member ID: ${member.id}<p>
+     </section>`;
+    });
+  }
+};
+
+const displayPublicMembersErrorMessage = (publicMembers) => {
+  if (publicMembers[5] === "Not Found") {
+    publicMembersIdsDiv.innerHTML = `<section class="individual-public-members">
+       <p>No Public Members Found<p>
+     </section>`;
+    console.log(publicMembers[5]);
+  } else {
+    publicMembersIdsDiv.innerHTML = publicMembers[5].map((member) => {
+      return `
+     <section class="individual-public-member">
+       <p>Public Member ID: ${member.id}<p>
+     </section>`;
+    });
+  }
+};
