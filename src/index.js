2e61936c34b584b7c27b30ef0babe2a2c541c6a5
Leah Winters <leah.winters24@gmail.com>
2020-11-24 16:00:39 -0500

    Fetch events data

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 var repoIdsDiv = document.querySelector(".repos-id-div");
 
 let reposData;
+let eventsData;
 
 reposData = fetch("https://api.github.com/orgs/BoomTownROI/repos")
   .then((data) => data.json())
   .then((data) => data)
   .catch((error) => console.log(error.message));
 
-Promise.all([reposData])
+eventsData = fetch("https://api.github.com/orgs/BoomTownROI/events")
+  .then((data) => data.json())
+  .then((data) => data)
+  .catch((error) => console.log(error.message));
+
+Promise.all([reposData, eventsData])
   .then((data) => {
-    repos = data;
+    reposData = data;
+    eventsData = data;
   })
   .then(() => {
-    displayReposIds(repos);
+    displayReposIds(reposData);
+    displayEventsIds(eventsData);
   })
   .catch((error) => {
     console.log(error.message);
@@ -20,10 +28,13 @@ Promise.all([reposData])
 
 const displayReposIds = (repos) => {
   repoIdsDiv.innerHTML = repos[0].map((repo) => {
-      console.log(repo.id);
-      return `
-        <section class="individual-repo">
-          <p>Repo ID: ${repo.id}<p>
-        </section>`;
-  })
+    return `
+      <section class="individual-repo">
+      <p>Repo ID: ${repo.id}<p>
+      </section>`;
+  });
+};
+
+const displayEventsIds = (events) => {
+  console.log(events[1], "events");
 };
