9fdf75e048c22fe2be59f9d39aeb2b8ed652db56
Leah Winters <leah.winters24@gmail.com>
2020-11-25 14:32:12 -0500

    Add notes to code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+// How I grab elements from the html and make them into variables for later use
 var getDataBtn = document.querySelector(".get-data-btn");
 var verifyDatesBtn = document.querySelector(".verify-dates-btn");
 var compareLengthBtn = document.querySelector(".compare-lengths-btn");
 
+// How I let the dom know when an event of a click happens, invoke the click handler
 getDataBtn.addEventListener("click", (event) => {
   handleClick(event);
 });
@@ -12,6 +14,7 @@ compareLengthBtn.addEventListener("click", (event) => {
   handleClick(event);
 });
 
+// How I dynamically can invoke functions based on what button was clicked
 const handleClick = (event) => {
   event.preventDefault();
   if (event.target.classList.contains("get-data-btn")) {
@@ -23,6 +26,7 @@ const handleClick = (event) => {
   }
 };
 
+// Helper function that fetches and displays all needed data/error messages
 const displayAllData = () => {
   displayReposIds();
   displayEventsIds();
@@ -32,23 +36,7 @@ const displayAllData = () => {
   displayPublicMembersErrorMessage();
 };
 
-const verifyDates = async () => {
-  const verifyDateDiv = document.querySelector(".verify-result-div");
-  let topLevelData = await fetchData("https://api.github.com/orgs/BoomTownROI");
-
-  let originalCreateDate = await topLevelData.created_at;
-  let newCreateDate = new Date(originalCreateDate);
-
-  let originalUpdateDate = await topLevelData.updated_at;
-  let newUpdateDate = new Date(originalUpdateDate);
-
-  if (newCreateDate < newUpdateDate) {
-    verifyDateDiv.innerHTML = `Update date is newer then create date.`;
-  } else {
-    verifyDateDiv.innerHTML = `Update date is older then create date.`;
-  }
-};
-
+// Reusable function that parses the data for me or gives me an error to help me determine where the fetch went wrong
 const fetchData = async (url) => {
   try {
     const response = await fetch(url);
@@ -59,6 +47,7 @@ const fetchData = async (url) => {
   }
 };
 
+// Displays repo id numbers after fetch
 const displayReposIds = async () => {
   const repoIdsDiv = document.querySelector(".repos-id-div");
   let reposData = await fetchData(
@@ -73,6 +62,7 @@ const displayReposIds = async () => {
   });
 };
 
+// displays event id numbers after fetch
 const displayEventsIds = async () => {
   const eventsIdsDiv = document.querySelector(".events-id-div");
   let eventsData = await fetchData(
@@ -87,6 +77,7 @@ const displayEventsIds = async () => {
   });
 };
 
+// displays hooks error after fetch
 const displayHooksErrorMessage = async () => {
   const hooksIdsDiv = document.querySelector(".hooks-id-div");
   let hooksData = await fetchData(
@@ -107,6 +98,7 @@ const displayHooksErrorMessage = async () => {
   }
 };
 
+// displays issues error after fetch
 const displayIssuesErrorMessage = async () => {
   const issuesIdsDiv = document.querySelector(".issues-id-div");
   let issuesData = await fetchData(
@@ -127,6 +119,7 @@ const displayIssuesErrorMessage = async () => {
   }
 };
 
+// displays members error after fetch
 const displayMembersErrorMessage = async () => {
   const membersIdsDiv = document.querySelector(".members-id-div");
   let membersData = await fetchData(
@@ -148,6 +141,7 @@ const displayMembersErrorMessage = async () => {
   }
 };
 
+// displays public members error after fetch
 const displayPublicMembersErrorMessage = async () => {
   const publicMembersIdsDiv = document.querySelector(".public-members-id-div");
   let publicMembersData = await fetchData(
@@ -168,13 +162,33 @@ const displayPublicMembersErrorMessage = async () => {
   }
 };
 
+const verifyDates = async () => {
+  const verifyDateDiv = document.querySelector(".verify-result-div");
+  let topLevelData = await fetchData("https://api.github.com/orgs/BoomTownROI");
+
+  // grabs the given date from the top level
+  let originalCreateDate = await topLevelData.created_at;
+  let originalUpdateDate = await topLevelData.updated_at;
+  
+  // converts the original given date to numbers I can use to compare to each other
+  let newCreateDate = new Date(originalCreateDate);
+  let newUpdateDate = new Date(originalUpdateDate);
+
+  // compares and displays text on dom based on the result
+  if (newCreateDate < newUpdateDate) {
+    verifyDateDiv.innerHTML = `Update date is newer then create date.`;
+  } else {
+    verifyDateDiv.innerHTML = `Update date is older then create date.`;
+  }
+};
+
 const getAllReposData = async () => {
-  // top level object
+  // Top level object
   const publicRepos = await fetchData(
     "https://api.github.com/orgs/BoomTownROI"
   );
 
-  // Since the api only serves 30 objects at a time for /repos, we need to know how many pages we need to pull
+  // Since the api only serves 30 objects at a time for /repos, I need to know how many pages I need to pull
   let cyclesNeeded = Math.ceil(publicRepos.public_repos / 30);
   let repoCount = 0;
 
@@ -183,6 +197,7 @@ const getAllReposData = async () => {
     repoCount += await iterateRepos(i);
   }
 
+  // Helper function invocation with the arguments of the total repo objects from all pages and the public repos value (number)
   compareReposLengths(repoCount, publicRepos.public_repos);
 };
 
@@ -190,6 +205,7 @@ const iterateRepos = async (i) => {
   let data = await fetchData(
     `https://api.github.com/orgs/BoomTownROI/repos?page=${i}`
   );
+  // Returns the total number of objects from fetch of each page
   return data.length;
 };
 
